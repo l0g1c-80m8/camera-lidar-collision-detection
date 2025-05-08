5649d538285671ad07a5b0b9bfe166523b8c7b6a
Rutvik Patel <rutvik.p4t3l@gmail.com>
2025-05-07 17:42:49 -0700

    implement Harris corner detection algorithm with non-maximum suppression

--- Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
+++ Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
@@ -27,13 +27,45 @@ void cornernessHarris()
     cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
     cv::convertScaleAbs(dst_norm, dst_norm_scaled);
 
+    vector<cv::KeyPoint> keypoints;
+    double maxOverlap = 0.0;
+    for (size_t j = 0; j < dst_norm.rows; j++) {
+        for (size_t i = 0; i < dst_norm.cols; i++) {
+            const int response = static_cast<int>(dst_norm.at<float>(j, i));
+            if (response > minResponse) {
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(i, j);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool bOverlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it) {
+                    const double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap) {
+                        bOverlap = true;
+                        if (newKeyPoint.response > (*it).response) {
+                            // if overlap is >t AND response is higher for new kpt
+                            *it = newKeyPoint; // replace old key point with new one
+                            break;             // quit loop over keypoints
+                        }
+                    }
+                }
+                if (!bOverlap) {
+                    // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        }
+    }
+
     // visualize results
     string windowName = "Harris Corner Detector Response Matrix";
     cv::namedWindow(windowName, 4);
     cv::imshow(windowName, dst_norm_scaled);
     cv::waitKey(0);
 
-    // TODO: Your task is to locate local maxima in the Harris response matrix 
+    // Your task is to locate local maxima in the Harris response matrix
     // and perform a non-maximum suppression (NMS) in a local neighborhood around 
     // each maximum. The resulting coordinates shall be stored in a list of keypoints 
     // of the type `vector<cv::KeyPoint>`.
